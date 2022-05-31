3624a551938d2adf9f16dcabb5007da5f7af0b6f
cruncher12 <94401008+cruncher12@users.noreply.github.com>
2022-05-31 08:11:49 -0600

    add more autoupdate

--- editor.js
+++ editor.js
@@ -7,6 +7,17 @@ btn.addEventListener("click", () => {
   iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
 });
 
+first.addEventListener('keyup',()=>{
+  var html = first.textContent;
+  iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+})
+
+first.addEventListener("paste", function(e) {
+        e.preventDefault();
+        var text = e.clipboardData.getData("text/plain");
+        document.execCommand("insertText", false, text);
+});
+
 window.onbeforeunload = function(){
   return 'Are you sure you want to leave?';
 };
