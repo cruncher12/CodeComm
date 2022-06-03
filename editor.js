a6f05dcd2f7a69224fe5e50cb04a7626a108fb5c
cruncher12 <94401008+cruncher12@users.noreply.github.com>
2022-06-03 14:24:18 -0600

    Add auto-updating toggle switch

--- editor.js
+++ editor.js
@@ -3,15 +3,31 @@ const iframe = document.querySelector('iframe');
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', () => {
-  var html = spacebox.textContent;
-  iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+  if (autoupdate === true) {
+    var html = spacebox.textContent;
+    iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+  }
 });
 
 document.getElementById('codebox').addEventListener('input', function () {
+  if (autoupdate === true) {
   var html = spacebox.textContent;
   iframe.src = 'data:text/html;charset=utf-8,' + encodeURI(html);
+  }
 })
 
 window.onbeforeunload = function () {
   return 'Are you sure you want to leave?';
 };
+
+document.addEventListener('DOMContentLoaded', function () {
+  var checkbox = document.querySelector('input[type="checkbox"]');
+
+  checkbox.addEventListener('change', function () {
+    if (checkbox.checked) {
+      autoupdate = true
+    } else {
+      autoupdate = false
+    }
+  });
+});
